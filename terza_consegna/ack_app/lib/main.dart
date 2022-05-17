4a802cb23c27ac39eab2c1309d65b20bbb2ef9ab
IqbalFofana <iqbalfofana@gmail.com>
2022-05-17 17:55:44 +0200

    added: al main il tema viola
    inoltre ho tolto const davanti al materialApp nel main

--- terza_consegna/ack_app/lib/main.dart
+++ terza_consegna/ack_app/lib/main.dart
@@ -10,8 +10,11 @@ import './pages/ui_home.dart';
 
 
 void main() {
-  runApp(const MaterialApp(
+  runApp( MaterialApp(
     title: 'Ori Live Results',
+    theme:  ThemeData(
+        primarySwatch: Colors.deepPurple,//Color.fromRGBO(37, 7, 107, 1.0),
+    ),
     home: UI_Home(),
   ));
 }
